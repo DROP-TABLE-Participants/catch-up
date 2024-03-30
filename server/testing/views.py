3d7f6815c741d23c9c2bdf2c55f89a2e3f23fbe2
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2024-03-31 01:22:55 +0200

    Add test scrape

--- server/testing/views.py
+++ server/testing/views.py
@@ -3,6 +3,13 @@ from rest_framework.response import Response
 
 from gpt.services.name_serializer import name_serializer
 
+import os
+from apify_client import ApifyClient
+from scrapers.services.sentiment import get_sentiment
+
+client = ApifyClient(os.getenv("APIFY_API_KEY"))
+
+
 class TestViewSet(ViewSet):
     def retrieve(self, request, pk=None):
         if pk is None or pk == "":
@@ -11,3 +18,40 @@ class TestViewSet(ViewSet):
         title = name_serializer(pk)
 
         return Response(title)
+
+    def create(self, request):
+        title = request.data.get("title")
+
+        if title is None or title == "":
+            return Response(status=400)
+
+        run_input = {
+            "debugMode": False,
+            "includeNSFW": False,
+            "maxComments": 0,
+            "maxCommunitiesCount": 0,
+            "maxItems": 10,
+            "maxPostCount": 10,
+            "maxUserCount": 0,
+            "proxy": {
+                "useApifyProxy": True,
+                "apifyProxyGroups": [
+                    "RESIDENTIAL"
+                ]
+            },
+            "scrollTimeout": 40,
+            "searches": [
+                title
+            ],
+            "skipComments": False,
+            "time": "month" if type == "monthly" else "week"
+        }
+
+        run = client.actor("trudax/reddit-scraper").call(run_input=run_input)
+        value = 0
+        for item in client.dataset(run["defaultDatasetId"]).iterate_items():
+            sentiment = get_sentiment(item["body"])
+
+            value += sentiment * item["upVotes"]
+
+        return Response(value)
