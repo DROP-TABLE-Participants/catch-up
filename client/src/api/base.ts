a395a24e505e6898586e39304cd1fc58e0ca4d8f
Atanas Pozharliev <ABPozharliev19@codingburgas.bg>
2024-03-31 08:46:21 +0300

    Change deployment

--- client/src/api/base.ts
+++ client/src/api/base.ts
@@ -16,7 +16,7 @@ import { Configuration } from "./configuration";
 // @ts-ignore
 import globalAxios, { AxiosRequestConfig, AxiosInstance } from 'axios';
 
-export const BASE_PATH = "http://localhost:8000".replace(/\/+$/, "");
+export const BASE_PATH = "https://catchupserver.azurewebsites.net".replace(/\/+$/, "");
 
 /**
  *
